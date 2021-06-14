1b50689cf43b20a7e50117d9c0e35e46f665e914
Facundo Acevedo <acevedofacundoemir@gmail.com>
2021-06-14 12:42:23 -0300

    menu de navegacion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import {NavBar} from "./components/NavBar/NavBar";
+import './components/NavBar/NavBar.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Esta es mi App con React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <h2>Esta es mi página!</h2>
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+export const NavBar = () => {
+    return(
+        <div className="navegacion">
+            <div className="contenedor">
+                <h1>Proyecto<span>React</span></h1>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Producto</li>
+                    <li>Carrito</li>
+                    <li>Nosotros</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
